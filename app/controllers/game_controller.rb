693ab85093c5b11a22b49025ab28d22698a76893
DunlapN <DunlapN@users.noreply.github.com>
2017-01-29 21:52:09 -0600

    practice done

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -9,12 +9,44 @@ class GameController < ApplicationController
 
     # Your logic here
 
+    computer_moveset = ["rock", "paper", "scissors"]
+    computer_move = computer_moveset.sample
+
+
+    if computer_move == @user_move
+      result = "Tie"
+    else
+
+      if computer_move == "rock"
+        if @user_move == "scissors"
+          result = "Computer Wins"
+        else
+          result = "You Win"
+        end
+      end
+
+      if computer_move == "scissors"
+        if @user_move == "paper"
+          result = "Computer Wins"
+        else
+          result = "You Win"
+        end
+      end
+
+      if computer_move == "paper"
+        if @user_move == "rock"
+          result = "Computer Wins"
+        else
+          result = "You Win"
+        end
+      end
+    end
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @computer_move = computer_move
 
-    @result = "Replace this string with the correct value."
+    @result = result
 
     # ===============================================================
     # Your code goes above.
